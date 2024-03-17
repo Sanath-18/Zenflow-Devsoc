cbf91a41c5e54e12205fc713ca650167da0e9d4f
Sanath-18 <sanath.skamath2022@vitstudent.ac.in>
2024-03-17 23:33:01 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Navbar } from './components/navbar'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import { Login } from "./pages/login";
+import { SignUp } from "./pages/signup.jsx";
+import { Home } from "./pages/home";
+import { Contact } from "./pages/contact";
+import { About } from "./pages/about";
 
-function App() {
-  return 
-  (
-    <Navbar />
-  )
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contactus" element={<Contact />} />
+          <Route path="/aboutus" element={<About />} />
+        </Routes>
+      </Router>
+         
+    </div>
+  );
 }
